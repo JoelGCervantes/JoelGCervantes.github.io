eba1302d062c28b5b81892c18a7441988752ba0c
Joel <g.joelcervantes@gmail.com>
2025-12-05 14:54:19 -0800

    got weather api to work, box shadow, section element done

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
   const nameEl = document.getElementById("name");
-  nameEl.textContent = "Joel Garcia-Cervantes";
-
   const weatherEl = document.getElementById("weather");
+
+  nameEl.textContent = "Joel Garcia-Cervantes";
 
   const apiKey = "6a0a3cbf0c8c2d1fd4a5dd9dedbc0c94";
   const lat = 45.5152;
   const lon = -122.6784;
   const units = "imperial";
-  const part = "minutely,hourly,daily";
 
-  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=${units}&exclude=${part}&appid=${apiKey}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
 
   fetch(url)
-    .then((response) => {
+    .then(async (response) => {
+      const data = await response.json();
+      console.log("Status:", response.status, data);
+
       if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+        throw new Error(
+          data.message || `HTTP error! status: ${response.status}`
+        );
       }
-      return response.json();
-    })
-    .then((data) => {
+
       const temp = data.main.temp;
       const desc = data.weather[0].description;
       const unitSymbol = units === "metric" ? "°C" : "°F";
 
-      weatherEl.textContent = `Current weather in ${city}: ${temp.toFixed(
-        1
-      )}${unitSymbol}, ${desc}`;
+      weatherEl.innerHTML = `
+  <span class="weather-label">Weather in Portland, OR</span><br />
+  <span class="weather-temp">${temp.toFixed(1)}${unitSymbol}</span><br />
+  <span class="weather-desc">${desc}</span>
+`;
     })
     .catch((err) => {
       console.error(err);
